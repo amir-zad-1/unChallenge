7ded1e17bb8a4ccf44c1fc0ecce9e7f157abd583
amir <am1hosseinzadeh@gmail.com>
2019-04-30 11:12:15 -0400

    add players endpoint

--- backend/src/controller/playerController.ts
+++ backend/src/controller/playerController.ts
@@ -1,4 +1,7 @@
 import {Request, Response} from "express";
+import {IPlayer} from "../database/player";
+import playerUtil from "../utility/playerUtil";
+import playerService from "../service/playerService";
 
 class PlayerController {
     public static getHandler(request: Request, response: Response): void {
@@ -6,7 +9,14 @@ class PlayerController {
     }
 
     public static postHandler(request: Request, response: Response): void {
-        response.json({newPlayer: true});
+        const player: IPlayer = playerUtil.getPlayer(request);
+        if (!playerUtil.isValid(player)) {
+            response.status(400).json({message: "invalid player data"});
+            return;
+        }
+        playerService.add(player).then((savePlayer) => {
+            response.status(201).json(playerUtil.toHttp(savePlayer));
+        });
     }
 }
 
--- backend/src/utility/playerUtil.ts
+++ backend/src/utility/playerUtil.ts
@@ -1,3 +1,39 @@
-export default class PlayerUtil {
+import {Request} from "express";
+import Player, {IPlayer} from "../database/player";
 
+const NAME_MIN_LENGTH = 3;
+const EMAIL_MIN_LENGTH = 5;
+
+export default class PlayerUtil {
+    public static isValid(player: IPlayer) {
+        return player.name &&
+            player.name.length > NAME_MIN_LENGTH &&
+            player.email &&
+            player.email.length > EMAIL_MIN_LENGTH; // todo: use validator package to validate player
+    }
+
+    public static getPlayer(request: Request): IPlayer {
+        const player: IPlayer = new Player();
+        try {
+            if (!request.body.name) {
+                throw new Error();
+            }
+            if (!request.body.email) {
+                throw new Error();
+            }
+            player.name = request.body.name;
+            player.email = request.body.email;
+            return player;
+        } catch (e) {
+            return player;
+        }
+    }
+
+    public static toHttp(player: IPlayer) {
+        return {
+            email: player.email,
+            id: player.id,
+            name: player.name,
+        };
+    }
 }
