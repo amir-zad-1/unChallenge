aa1db0eff108d786668ad4537e49d036148b9cf4
amir <am1hosseinzadeh@gmail.com>
2019-04-30 14:34:17 -0400

    improve integration tests

--- backend/test/integration/gameSession.ts
+++ backend/test/integration/gameSession.ts
@@ -9,6 +9,10 @@ import config from "../../src/config";
 const assert = chai.assert;
 
 const API_URL = `http://localhost:${config.server.port}/gamesessions`;
+const gameSessionPostData = {
+    name: "NAME",
+    players: ["000000000000000000000000", "111111111111111111111111"],
+};
 
 describe("/gamesessions", () => {
 
@@ -24,20 +28,13 @@ describe("/gamesessions", () => {
     });
 
     it("POST returns 201 for valid game session data", (done) => {
-        const postData = {
-            name: "NAME",
-            players: ["000000000000000000000000", "111111111111111111111111"],
-        };
-        chai.request(API_URL).post("/").send(postData).end((error, response) => {
-            if (error) {
-                return done(error);
-            }
-            assert.equal(response.body.name, postData.name);
-            assert.exists(response.body.id);
-            assert.equal(response.body.totalFeedback, 0);
-            assert.equal(response.status, 201);
+        postGameSession().then((gameSessionResponse: any) => {
+            assert.equal(gameSessionResponse.body.name, gameSessionPostData.name);
+            assert.exists(gameSessionResponse.body.id);
+            assert.equal(gameSessionResponse.body.totalFeedback, 0);
+            assert.equal(gameSessionResponse.status, 201);
             done();
-        });
+        }).catch((error) => done(error));
     });
 
     it("POST returns 400 for invalid game session data", (done) => {
@@ -53,9 +50,16 @@ describe("/gamesessions", () => {
         });
     });
 
-    describe("/gamesessions/:id/feedbacks", () => {
-        it("GET returns 200 and list of game session feedbacks", (done) => {
-            chai.request(API_URL).get("/").end((error, response) => {
+    describe("/:id/feedbacks", () => {
+        let gameSession: any;
+        before((done) => {
+            postGameSession().then((gameSessionResponse: any) => {
+                gameSession = gameSessionResponse.body;
+                done();
+            });
+        });
+        it("GET returns 200 and empty list of game session feedbacks", (done) => {
+            chai.request(API_URL).get(`/${gameSession.id}/feedbacks`).end((error, response) => {
                 if (error) {
                     return done(error);
                 }
@@ -64,6 +68,26 @@ describe("/gamesessions", () => {
                 done();
             });
         });
+        //
+        //     it("POST returns 201 for feedbacks", (done) => {
+        //         assert.equal(Array.isArray(getResponse.body), true);
+        //         assert.equal(getResponse.status, 200);
+        //         done();
+        //     });
     });
 
 });
+
+const postGameSession = () => {
+    return new Promise((resolve, reject) => {
+        chai.request(API_URL).post("/").send(gameSessionPostData).end((postError, postResponse) => {
+            if (postError) {
+                return reject(postError);
+            }
+            resolve({
+                body: postResponse.body,
+                status: postResponse.status,
+            });
+        });
+    });
+};
